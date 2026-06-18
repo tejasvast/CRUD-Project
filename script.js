1fb8403a3ae9b827583a85afdd58e27ca079967f
tejasvast <103351784+tejasvast@users.noreply.github.com>
2026-06-18 22:25:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const formDiv = document.querySelector(".box");
+
+const addtask = document.querySelector('.addtask')
+
+addtask.addEventListener('click',() =>{
+    formDiv.style.display = "flex";
+})

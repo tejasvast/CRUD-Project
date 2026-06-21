a2e76a1a57f3a0a5eb209f26662d1d511f9637b9
tejasvast <103351784+tejasvast@users.noreply.github.com>
2026-06-21 15:31:38 +0530

    Enhance task form functionality and UI updates
    Added event listeners for closing the form and submitting tasks. Updated UI rendering for task display.

--- script.js
+++ script.js
@@ -1,7 +1,56 @@
-const formDiv = document.querySelector(".box");
+const formDiv = document.querySelector(".form");
 
 const addtask = document.querySelector('.addtask')
+
+const closebtn = document.querySelector('.top')
+
+const form = document.querySelector('form')
+const taskdiv = document.querySelector('.task')
+
+const taskcoll = []
 
 addtask.addEventListener('click',() =>{
     formDiv.style.display = "flex";
 })
+
+closebtn.addEventListener('click',()=>{
+    formDiv.style.display = "none";
+})
+
+form.addEventListener('submit',()=>{
+    event.preventDefault();
+
+    let taskname = event.target[0].value
+    let desc = event.target[1].value
+    
+
+    let obj ={
+        taskname,
+        desc,
+    }
+
+    taskcoll.push(obj)
+    ui()
+    form.reset();
+    formDiv.style.display ="none";
+
+})
+
+
+let ui = () => {
+    taskdiv.innerHTML= "";
+
+    taskcoll.forEach((elem,index)=> {
+        taskdiv.innerHTML +=`<div class="taskbox">
+        <div class="text">
+            <h1>${elem.taskname}</h1>
+            <h3>${elem.desc}</h3>
+        </div>
+        <div class="btn">
+            <button id = "Comp">Completed</button>
+            <button id = "Del">Delete</button>
+        </div>    
+        </div>`
+    });
+
+}
